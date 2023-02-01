69f1da2059625e4d0daf87829df6e6f5c063cd09
Gwan <slavik.art15@gmail.com>
2023-02-01 14:45:31 +0300

    add all files

--- calc.js
+++ calc.js
@@ -1,31 +1,45 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+global.document = new JSDOM('calc.html').window.document;
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 80;
+
+app.listen(PORT, () => {
+    console.log('Server has been started...');
+});
+
+
 const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
 
-buttons.forEach((item) => {
-    item.onclick = () => {
-        if(item.id === 'clear') {
-            display.innerText = '';
-        } else if(item.id === 'back') {
-            let string = display.innerText.toString();
-            display.innerText = string.substr(0, string.length-1);
-        } else if(display.innerText !== '' && item.id === 'equal') {
-            display.innerText = eval(display.innerText);
-        } else if(item.id === '√') {
-            display.innerText = Math.sqrt(display.innerText);
-        } else if(item.id === 'plusm') {
-            if(display.innerText === ''){
-                display.innerText = '-';
-            } else if(display.innerText === '-'){
-                display.innerText = '';
-            } 
-        } else if(display.innerText === '' && item.id ==='equal') {
-            display.innerText = 'Empty';
-            setTimeout(() => (display.innerText = ''), 2000);
-        } else {
-            display.innerText += item.id;
-        }
-    }
-})
+ buttons.forEach((item) => {
+     item.onclick = () => {
+         if(item.id === 'clear') {
+             display.innerText = '';
+         } else if(item.id === 'back') {
+             let string = display.innerText.toString();
+             display.innerText = string.substr(0, string.length-1);
+         } else if(display.innerText !== '' && item.id === 'equal') {
+             display.innerText = eval(display.innerText);
+         } else if(item.id === '√') {
+             display.innerText = Math.sqrt(display.innerText);
+         } else if(item.id === 'plusm') {
+             if(display.innerText === ''){
+                 display.innerText = '-';
+             } else if(display.innerText === '-'){
+                 display.innerText = '';
+             } 
+         } else if(display.innerText === '' && item.id ==='equal') {
+             display.innerText = 'Empty';
+             setTimeout(() => (display.innerText = ''), 2000);
+         } else {
+             display.innerText += item.id;
+         }
+     }
+ })
 
 const themeToggleBtn = document.querySelector('.theme-toggler');
 const calculator = document.querySelector('.calculator');
@@ -37,4 +51,3 @@ themeToggleBtn.onclick = () => {
     themeToggleBtn.classList.toggle('active');
     isDark = !isDark;
 }
-
